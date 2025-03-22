f2c4ec49cec6aaa4252b96313994511d89ac68dd
AndreOganesian <ao367@cornell.edu>
2025-03-22 17:07:18 -0400

    Fix split bounds

--- split_data.py
+++ split_data.py
@@ -1,7 +1,5 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
 
-# Read the data
 df = pd.read_csv('labelled_tweets.csv', 
                  quoting=1,
                  escapechar='\\',
@@ -16,17 +14,15 @@ df = pd.read_csv('labelled_tweets.csv',
 df['created_at'] = pd.to_datetime(df['created_at'])
 
 total_len = len(df)
-train_size = int(total_len * 0.7)
-val_size = int(total_len * 0.15)
-test_size = total_len - train_size - val_size
+train_bound = int(total_len * 0.7)
+train_val_bound = int(total_len * 0.85)
 
-train = df.iloc[:train_size]
-val = df.iloc[train_size:(train_size + val_size)]
-test = df.iloc[(train_size + val_size):]
-# Save to CSV files
+train = df.iloc[:train_bound]
+val = df.iloc[train_bound:train_val_bound]
+test = df.iloc[train_val_bound:]
+
 train.to_csv('train.csv')
 val.to_csv('val.csv')
 test.to_csv('test.csv')
 
-# Print the sizes of the splits
-print(f"Total: {total_len}, Train: {train_size}, Validation: {val_size}, Test: {test_size}")
+print(f"Total: {total_len}, Train: {train_bound}, Validation: {train_val_bound - train_bound}, Test: {total_len - train_val_bound}")
